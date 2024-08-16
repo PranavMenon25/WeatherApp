744dbf0e99b5774ada6b4a60ce89ad680c37cc48
PranavMenon25 <pranavmenon2019@gmail.com>
2024-08-17 02:55:36 +0530

    Third Commit

--- src/SearchBox.jsx
+++ src/SearchBox.jsx
@@ -4,13 +4,10 @@ import "./SearchBox.css"
 import { useState } from 'react';
 // import { API_KEY } from './temp';
 
-const url = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={0fc26a4ed1acc4d32412dd3c01f404dc}";
-
 export default function SearchBox(){
     let [city, setCity] = useState('');
     const API_URL = "https://api.openweathermap.org/data/2.5/weather";
     const API_KEY = import.meta.env.VITE_API_KEY;
-    console.log(API_KEY);
     let getWeatherInfo = async ()=>{
         console.log(city);
         let res = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
